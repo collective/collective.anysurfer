e2c5e0c0adb476b54e9da67580b8b182266c68ac
Christophe Boulanger <christophe.boulanger@imio.be>
2020-05-27 16:36:23 +0200

    Test a dataadapter to view richtext widget and to register in registry just an unicode value (and not a richtext object.. not possible)

--- src/collective/anysurfer/browser/controlpanel.py
+++ src/collective/anysurfer/browser/controlpanel.py
@@ -2,6 +2,11 @@
 from collective.anysurfer import _
 from collective.anysurfer.interfaces import IAnysurferSettings
 from plone.app.registry.browser import controlpanel
+# from plone.app.textfield.value import RichTextValue
+# from plone.app.z3cform.wysiwyg.widget import IWysiwygWidget
+# from z3c.form import converter
+
+# import zope.component
 
 
 class AnysurferSettingsEditForm(controlpanel.RegistryEditForm):
@@ -22,3 +27,13 @@ class AnysurferSettingsControlPanel(controlpanel.ControlPanelFormWrapper):
     """Control panel form wrapper."""
 
     form = AnysurferSettingsEditForm
+
+
+# class RichtextDataConverter(converter.FieldDataConverter):
+#     zope.component.adapts(zope.schema.interfaces.IText, IWysiwygWidget)
+#
+#     def toWidgetValue(self, value):
+#         return RichTextValue(value, 'text/plain', 'text/html')
+#
+#
+# zope.component.provideAdapter(RichtextDataConverter)
