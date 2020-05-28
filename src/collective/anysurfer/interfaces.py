f980ddd7fc74eed37bc3df27da67477e57ab2ee9
Laurent Lasudry <lasudry@gmail.com>
2020-05-28 11:22:52 +0200

    Cleanup

--- src/collective/anysurfer/interfaces.py
+++ src/collective/anysurfer/interfaces.py
@@ -3,10 +3,8 @@ from collective.anysurfer import _
 from collective.anysurfer.utils import get_default_text_translations
 from collective.z3cform.datagridfield import DataGridFieldFactory
 from collective.z3cform.datagridfield.registry import DictRow
-# from plone.autoform import directives as form
 from plone.app.textfield import RichText
 from plone.autoform.directives import widget
-# from plone.app.z3cform.wysiwyg import WysiwygFieldWidget
 from plone.theme.interfaces import IDefaultPloneLayer
 from zope import schema
 from zope.interface import Interface
@@ -20,7 +18,6 @@ class ISkinTemplateView(Interface):
     """Marker interface added when CMF skin template is rendered."""
 
 
-# Interface
 class ITextRowSchema(Interface):
 
     language = schema.TextLine(
@@ -28,7 +25,6 @@ class ITextRowSchema(Interface):
         description=_(u'Enter the language code. Ex.: en'),
     )
 
-#    form.widget("text", WysiwygFieldWidget)
     text = RichText(
         title=_(u"Text"),
     )
