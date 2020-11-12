2664beae0a0457fadc57f794c3f3f5c0a988e053
Christophe Boulanger <christophe.boulanger@imio.be>
2020-11-12 15:51:17 +0100

    Add HAS_PLONE_5 global var.

--- src/collective/anysurfer/__init__.py
+++ src/collective/anysurfer/__init__.py
@@ -1,6 +1,9 @@
 # -*- coding: utf-8 -*-
+from plone import api
 from zope.i18nmessageid import MessageFactory
 
+
+HAS_PLONE_5 = api.env.plone_version().startswith('5')
 _ = MessageFactory("collective.anysurfer")
 
 
